da182ab48678a1e25534417cb6125c8be1761b23
Manu <mweiss1377@gmail.com>
2025-09-28 20:26:55 +0200

    add and finish exercice 1

--- Phase_3/Eval8_débuter_en_JS/Eval8_Eval8_débuter_en_JS_SY_P3/exercice 2/assets/script.js
+++ Phase_3/Eval8_débuter_en_JS/Eval8_Eval8_débuter_en_JS_SY_P3/exercice 2/assets/script.js
@@ -19,7 +19,7 @@ function genererTableau() {
     celluleJour.textContent = planningJours[i].jour;
 
     const celluleStatut = document.createElement("td");
-    celluleStatut.textContent = planningJours[i].statut ? "Réservé" : "Libre";
+    celluleStatut.textContent = planningJours[i].statut;
     celluleStatut.className = planningJours[i].statut ? "red" : "green";
 
     // Ajout de l’interaction
@@ -33,5 +33,41 @@ function genererTableau() {
     table.appendChild(ligne);
   }
 }
+
+// Bouton Calculer
+document.getElementById("btnCalcul").addEventListener("click", () => {
+  const libres = planningJours.filter((j) => !j.statut).length;
+  const reserves = planningJours.length - libres;
+  alert(`Libre : ${libres} | Réservé : ${reserves}`);
+});
+
+// Bouton Réinitialiser
+document.getElementById("btnReset").addEventListener("click", () => {
+  planningJours.forEach((j) => (j.statut = false));
+  genererTableau();
+});
+
+// Bouton Réservation Aléatoire
+document.getElementById("btnRandom").addEventListener("click", () => {
+  let n = parseInt(prompt("Combien de jours voulez-vous réserver ?"));
+  if (isNaN(n) || n < 0 || n > 5) {
+    alert("Entrée invalide");
+    return;
+  }
+
+  planningJours.forEach((j) => (j.statut = false)); // reset
+
+  let indices = [];
+  while (indices.length < n) {
+    let rand = Math.floor(Math.random() * 5);
+    if (!indices.includes(rand)) {
+      indices.push(rand);
+      planningJours[rand].statut = true;
+    }
+  }
+
+  genererTableau();
+});
+
 // Générer tableau au chargement
 genererTableau();
