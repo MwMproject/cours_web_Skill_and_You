80131195de63251a1241c9822c249301df9d3cfc
Manu <mweiss1377@gmail.com>
2026-04-08 19:45:28 +0200

    add jpg filter in parseDir + comments on Image.php and index.php

--- Phase_3/Eval18_PHP_adv_1/Eval18_PHP_adv_1/index.php
+++ Phase_3/Eval18_PHP_adv_1/Eval18_PHP_adv_1/index.php
@@ -1,22 +1,28 @@
 <?php
-// On détermine le chemin du répertoire images/
-// la constante PHP __FILE__ restitue le chemin du fichier en cours
-// la fonction dirname restitue le chemin du répertoire du fichier
+
+// Chemin absolu vers le répertoire des images
+// dirname(__FILE__) retourne le dossier où se trouve ce fichier
 $image_dir = dirname(__FILE__) . '/images/';
-// inclusion classe Image
+
+// On inclut la classe Image
 require('class/Image.php');
-// instanciation classe Image
+
+// On crée un objet Image pour utiliser ses méthodes
 $image = new Image();
-// parse images/ et récupérons liste des fichiers et répertoires
+
+// On parcourt le répertoire et on récupère la liste des images et des séries
 $image_data = $image->parseDir($image_dir);
 $subdir_list = $image_data['subdir_list'];
 $image_list = $image_data['image_list'];
-// $image_list est 'null' si aucune image n'est présente dans le répertoire
+
+// Si aucune image trouvée, on affiche un message et on arrête le script
 if(!isset($image_list)) {
     echo 'Aucune image dans le répertoire';
     exit;
 }
-// Créons les sous-répertoires
+
+// On crée les sous-répertoires pour chaque série
 $subdir_list = $image->createSubdir($image_dir, $subdir_list);
-// puis déplaçons les images dans ces sous-répertoires
+
+// On déplace chaque image dans le sous-répertoire correspondant à sa série
 $image->moveImage($image_dir, $image_list, $subdir_list);
