30039ddc91015d56d091e9fcfc3ed2b71fe46189
Manu <mweiss1377@gmail.com>
2026-08-27 13:50:27 +0200

    first flask '/' and '/login' routes added

--- Phase_5/Eval30_Python/eval30_python/app.py
+++ Phase_5/Eval30_Python/eval30_python/app.py
@@ -1,3 +1,11 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def accueil():
+    return "Bonjour utilisateur anonyme"
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return "Page de connexion"
